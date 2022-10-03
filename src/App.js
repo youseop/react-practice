fa8f2e843aa73f71d34859e474395c29a8df6995
Youseop Lee <youseop.lee@voyagerx.com>
2022-10-04 01:01:35 +0900

    doc crud complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,81 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [newAge, setNewAge] = useState(0);
+  const [newName, setNewName] = useState(0);
+
   const usersCollectionRef = collection(db, "users");
 
   useEffect(() => {
     const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
+      const data = await getDocs(
+        query(usersCollectionRef, orderBy("timestamp", "desc"))
+      );
       setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getUsers();
   }, [usersCollectionRef]);
 
-  const createUser = () => {
-    console.log("!23");
+  const createUser = async () => {
+    console.log(newName, newAge);
+    await addDoc(usersCollectionRef, {
+      name: newName,
+      age: Number(newAge),
+      timestamp: new Date(),
+    });
+    setNewAge(0);
+    setNewName("");
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { age: age + 1 };
+
+    await updateDoc(userDoc, newFields);
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+
+    await deleteDoc(userDoc);
   };
 
   return (
     <div className="App">
-      <input placeholder="Name..." />
-      <input type="number" placeholder="Age..." />
+      <input
+        onChange={(event) => setNewName(event.target.value)}
+        value={newName}
+      />
+      <input
+        type="number"
+        onChange={(event) => setNewAge(event.target.value)}
+        value={newAge}
+      />
 
       <button onClick={createUser}> Create User </button>
 
       {users.map((user, index) => {
+        const { name, age, id } = user;
         return (
-          <div key={`${user.name}_${user.age}_${index}`}>
-            {`name: ${user.name}`}
+          <div key={`${name}_${age}_${index}`}>
+            {`name: ${name}`}
             <br />
-            {`age: ${user.age}`}
+            {`age: ${age}`}
+            <button onClick={() => updateUser(id, age)}>add age</button>
+            <button onClick={() => deleteUser(id)}>delete user</button>
           </div>
         );
       })}
