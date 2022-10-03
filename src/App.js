143b5f7b6cb95a65c1708e08cf27f35480128af7
Youseop Lee <youseop.lee@voyagerx.com>
2022-10-04 00:42:09 +0900

    add firebase config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, [usersCollectionRef]);
+
+  const createUser = () => {
+    console.log("!23");
+  };
+
   return (
     <div className="App">
-      hi there
-    
+      <input placeholder="Name..." />
+      <input type="number" placeholder="Age..." />
+
+      <button onClick={createUser}> Create User </button>
+
+      {users.map((user, index) => {
+        return (
+          <div key={`${user.name}_${user.age}_${index}`}>
+            {`name: ${user.name}`}
+            <br />
+            {`age: ${user.age}`}
+          </div>
+        );
+      })}
     </div>
   );
 }
